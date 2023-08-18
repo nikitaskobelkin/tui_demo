53b5bc09077f6f098a31fdc5cb379ccc7e6f4d8a
Nikita Skobelkin <nikita@apple.com>
2023-08-18 10:15:39 +0100

    Initial Commit

---
+++ TUI/TUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUIApp.swift
+//  TUI
+//
+//  Created by Nikita Skobelkin on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
